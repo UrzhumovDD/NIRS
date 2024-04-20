bf30ad1ee1dec5ab19dcd46cb8e7d0f2bb1bc0c1
denis <denis.urzhumov.02@mail.ru>
2024-04-20 14:54:38 +0300

    change legende module

--- Ssource.f90
+++ Ssource.f90
@@ -1,34 +1,43 @@
-module Legendre
-use, intrinsic :: iso_fortran_env, only: dp=>real64
-implicit none
+module legendre
+
+    use, intrinsic :: iso_fortran_env, only: dp => real64
+    
+    implicit none
+
+    private
+    public  :: Pn
+    
     contains
-!Calculating legendre's polynomial of the n order in x
-real(dp) function Pn(n,x) result(c)
-real(dp), intent(in   )     :: x
-integer,  intent(in   )     :: n
-real(dp)                    :: a, b
-integer                     :: i
-!
-if ( n == 0 ) then
-    c = 1.0_dp
-!
-else if ( n == 1 ) then
-    c = x
-!
-else
-    a = 1.0_dp
-    b = x
-!
-do i = 2, n
-!
-    c = b * x * ( 2.0_dp*i - 1.0_dp ) / i - a * ( i - 1.0_dp ) / i
-    a = b
-    b = c
-!
-end do
-end if
-end function Pn
-end module Legendre
+
+    !> Calculate Legendre polynomial of the n-th order at the point x
+    pure elemental real(dp) function Pn(n, x) 
+        
+        real(dp), intent(in)    :: x        !! argument
+        integer,  intent(in)    :: n        !! polynomial order
+        real(dp)                :: a, b     !  variables for recurrence relation: a = P_{n-2}, b = P_{n-1}
+        integer                 :: i        !  loop counter
+
+        ! Bonnet's recursion formula
+        if (n < 0) then
+            error stop "Negative order of Legendre polynomial"
+        else if (n == 0) then
+            Pn = 1.0_dp
+        else if (n == 1) then
+            Pn = x
+        else
+            a = 1.0_dp
+            b = x
+            do i = 2, n
+                Pn = b * x * real(2 * i - 1, dp) / real(i, dp) - a * real(i - 1, dp) / real(i, dp)
+                a = b
+                b = Pn
+            end do
+        end if
+
+    end function Pn
+
+end module legendre
+
 !
 module Calc_of_nodes
 use legendre, only : Pn
