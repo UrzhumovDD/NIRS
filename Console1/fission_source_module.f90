36df0d1e546d97028b3cc0ec7ee811c37a019cb1
denis <denis.urzhumov.02@mail.ru>
2024-04-21 15:50:52 +0300

    commented program in scat_source and add fission source module

---
+++ Console1/fission_source_module.f90
@@ -0,0 +1,65 @@
+module Fission_source
+    
+    use Integration_with_weights, only: Integral
+    use Calc_of_nodes,            only: C_o_n
+    use Calc_of_weights,          only: C_o_W
+    use, intrinsic :: iso_fortran_env, only: dp=>real64
+    
+    implicit none
+    private
+    public :: Fsource
+    
+    contains
+    
+    subroutine Fsource(Flux, Nuf, cross_sec_fis, Hi, Keff, res )
+    real(dp),              intent(in   ) :: Flux(:,:,:), Nuf(:,:), cross_sec_fis(:,:), Hi(:)
+    real(dp), allocatable, intent(inout) :: res(:,:)
+    real(dp), allocatable                :: nodes(:), weights(:), Integr(:,:)
+    real(dp)                             :: Keff
+    integer                              :: i, j
+    
+    call C_o_n( SIZE(Flux, 2), nodes)
+    call C_o_w( nodes, weights)
+    allocate( Integr( SIZE(Flux, 1), SIZE(Flux, 3) ) )
+    allocate( res( SIZE(Flux, 1), SIZE(Flux, 3) ) )
+    ! integration and dividing by 2
+    do i = 1, SIZE(Flux, 1)
+        do j = 1, SIZE(Flux, 3)
+            Integr(i,j) = Integral( Flux( i,:, j ) , weights) * 0.5_dp
+        end do
+    end do
+    do i = 1, SIZE(Flux, 1)
+        do j = 1, SIZE(Flux, 3)
+            res(i,j) = Hi(j) / Keff * SUM( Integr(i, :) * Nuf(i, :) * cross_sec_fis(i, :) )
+        end do
+    end do
+    
+    end subroutine Fsource
+    
+end module Fission_source
+!
+program FS
+
+    use Fission_source, only : Fsource
+    use, intrinsic        :: iso_fortran_env, only: dp=>real64
+    
+    implicit none 
+    
+    real(dp), allocatable :: Flux(:,:,:), Nuf(:,:), cross_sec_fis(:,:), Hi(:), Fiss_S(:,:)
+    real(dp)              :: Keff
+    integer               :: ncord, nenergy, nangles
+    
+    ncord = 3
+    nenergy = 5
+    nangles = 5
+    Keff = 1.0_dp
+    allocate( Flux(ncord, nangles, nenergy ), source = 1.0_dp )
+    allocate( Nuf(ncord, nenergy ), source = 1.0_dp )
+    allocate( cross_sec_fis(ncord, nenergy ), source = 1.0_dp )
+    allocate( Hi( nenergy ), source = 1.0_dp )
+    call Fsource(Flux, Nuf, cross_sec_fis, Hi, Keff, Fiss_S )
+    deallocate(Flux)
+    deallocate(Nuf)
+    deallocate(cross_sec_fis)
+    deallocate(Hi)
+end program FS
--- Console1/scaterring_source_module.f90
+++ Console1/scaterring_source_module.f90
@@ -43,60 +43,60 @@ module Scaterring_source
     
 end module Scaterring_source
 
-program test
-
-    use Scaterring_source, only : Ssource
-    use Calc_of_nodes, only     : C_o_n
-    use Poly_in_nodes, only     : All_poly
-    use, intrinsic        :: iso_fortran_env, only: dp=>real64
-    
-    implicit none
-    
-    real(dp), allocatable :: nodes(:), Flux(:,:,:), Cross_sec_scat(:,:,:,:), SS(:,:,:), Poly(:,:), SS_an(:,:,:)
-    real(dp)              :: integr_analytic(6)
-    integer               :: order,ncord,nenergy,nangles, i, j, k,energy
-    
-    !values for variables used to check module
-    order = 5
-    ncord = 3
-    nenergy = 5
-    energy = 1
-    nangles = 5
-    call C_o_n(nangles,nodes)
-    allocate(Flux(ncord, nangles, nenergy))
-    do i = 1,SIZE(Flux,1)
-        do k = 1,SIZE(Flux,3)
-            Flux(i,:,k) = COS(nodes)
-        end do
-    end do
-    allocate(Cross_sec_scat(ncord,nenergy,nenergy,order + 1),source = 1.0_dp)
-    !analyticaly calculated integral of Pn*cos(mu)
-    integr_analytic(1) = 2_dp * SIN(1.0_dp)
-    integr_analytic(2) = 0_dp
-    integr_analytic(3) = 6_dp * COS(1.0_dp) - 4_dp * SIN(1.0_dp)
-    integr_analytic(4) = 0_dp
-    integr_analytic(5) = 122_dp * SIN(1.0_dp) - 190_dp * COS(1.0_dp)
-    integr_analytic(6) = 0_dp
-    allocate( Poly(order + 1, nangles) )
-    call All_poly( Poly, nodes)
-    call Ssource( Flux, Cross_sec_scat, order, energy, SS)
-    allocate( SS_an(ncord, nangles, nenergy ), source = 0.0_dp)
-    do i = 1, SIZE(Flux, 1)
-        do j = 1, order + 1
-           do k = 1, SIZE(Flux, 3)
-               SS_an(i,:,k) = SS_an(i,:,k) + Poly(j,:) * Cross_sec_scat(i, k, energy, j) * ( 2_dp * ( j - 1_dp ) + 1_dp) / 2_dp * integr_analytic(j)
-           end do
-      end do
-    end do
-    !absolute difference
-    write(*,*) MAXVAL(ABS( SS_an - SS ) )
-    !relative difference
-    write(*,*) MAXVAL(ABS( (SS_an - SS) / SS ) )
-    deallocate(Flux)
-    deallocate(Cross_sec_scat)
-    deallocate(SS_an)
-    deallocate(nodes)
-    deallocate(Poly)
-    deallocate(SS)
-    
-end program test
+!program test
+!
+!    use Scaterring_source, only : Ssource
+!    use Calc_of_nodes, only     : C_o_n
+!    use Poly_in_nodes, only     : All_poly
+!    use, intrinsic        :: iso_fortran_env, only: dp=>real64
+!    
+!    implicit none
+!    
+!    real(dp), allocatable :: nodes(:), Flux(:,:,:), Cross_sec_scat(:,:,:,:), SS(:,:,:), Poly(:,:), SS_an(:,:,:)
+!    real(dp)              :: integr_analytic(6)
+!    integer               :: order,ncord,nenergy,nangles, i, j, k,energy
+!    
+!    !values for variables used to check module
+!    order = 5
+!    ncord = 3
+!    nenergy = 5
+!    energy = 1
+!    nangles = 5
+!    call C_o_n(nangles,nodes)
+!    allocate(Flux(ncord, nangles, nenergy))
+!    do i = 1,SIZE(Flux,1)
+!        do k = 1,SIZE(Flux,3)
+!            Flux(i,:,k) = COS(nodes)
+!        end do
+!    end do
+!    allocate(Cross_sec_scat(ncord,nenergy,nenergy,order + 1),source = 1.0_dp)
+!    !analyticaly calculated integral of Pn*cos(mu)
+!    integr_analytic(1) = 2_dp * SIN(1.0_dp)
+!    integr_analytic(2) = 0_dp
+!    integr_analytic(3) = 6_dp * COS(1.0_dp) - 4_dp * SIN(1.0_dp)
+!    integr_analytic(4) = 0_dp
+!    integr_analytic(5) = 122_dp * SIN(1.0_dp) - 190_dp * COS(1.0_dp)
+!    integr_analytic(6) = 0_dp
+!    allocate( Poly(order + 1, nangles) )
+!    call All_poly( Poly, nodes)
+!    call Ssource( Flux, Cross_sec_scat, order, energy, SS)
+!    allocate( SS_an(ncord, nangles, nenergy ), source = 0.0_dp)
+!    do i = 1, SIZE(Flux, 1)
+!        do j = 1, order + 1
+!           do k = 1, SIZE(Flux, 3)
+!               SS_an(i,:,k) = SS_an(i,:,k) + Poly(j,:) * Cross_sec_scat(i, k, energy, j) * ( 2_dp * ( j - 1_dp ) + 1_dp) / 2_dp * integr_analytic(j)
+!           end do
+!      end do
+!    end do
+!    !absolute difference
+!    write(*,*) MAXVAL(ABS( SS_an - SS ) )
+!    !relative difference
+!    write(*,*) MAXVAL(ABS( (SS_an - SS) / SS ) )
+!    deallocate(Flux)
+!    deallocate(Cross_sec_scat)
+!    deallocate(SS_an)
+!    deallocate(nodes)
+!    deallocate(Poly)
+!    deallocate(SS)
+!    
+!end program test
